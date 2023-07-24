828039b2891bd44469a69d221d0ccc6492664781
farrokh03 <140150581+farrokh03@users.noreply.github.com>
2023-07-24 14:36:25 +0330

    responsive
    صفحه رو کوچیک بزرگ کن

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 let x = document.getElementById('circle');
 let y = document.getElementById('icon');
 let m = document.getElementById('login');
-
+document.getElementById('burgermenu').onclick =sidemenu;
+let n=document.getElementById('burgermenu');
 function button(){
     if( x.className === "circle"){
         
@@ -27,3 +28,25 @@ function button(){
         m.className="loginform";
     }
 }
+if(screen.width<'992 px')
+{
+    document.getElementById('sidebar').style.display='none';
+}
+
+function sidemenu(){
+    if(n.innerHTML==="menu_open")
+    {
+        document.getElementById('sidebar').style.display='block';
+        document.getElementById('sidebar').style.right='0';
+        document.getElementById('sidebar').style.animationName ='opensidemenu';
+        document.getElementById('burgermenu').innerHTML = "cancel"; 
+    }
+    else{
+        document.getElementById('sidebar').style.right='-40%';
+        document.getElementById('sidebar').style.animationName ='closesidemenu';
+        document.getElementById('burgermenu').innerHTML = "menu_open";
+       // document.getElementById('sidebar').style.display='none';
+    }    
+}
+
+

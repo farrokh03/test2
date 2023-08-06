e3a509c0e01327612c7278a42bf9bf115dabd682
farrokh03 <140150581+farrokh03@users.noreply.github.com>
2023-08-06 16:58:00 +0330

    drost shd
    <>

--- moreblog/blog.js
+++ moreblog/blog.js
@@ -4,28 +4,8 @@ let buttons = document.getElementById('buttons');
 let menu = document.getElementById('menu');
 let nav = document.getElementById('navbar');
 
-box.onmousemove = hover;
-box.onmouseleave = leave;
 menu.onclick = dropdownmenu;
 
-
-function hover(){
-    box.style.animationName = 'changesize';
-    box.style.width='305px';
-    box.style.height='410px';
-    title.style.display ='block';
-    title.className="tracking-in-contract-bck";
-    buttons.style.display = 'flex';
-    buttons.style.bottom='-15%';
-}
-function leave(){
-    box.style.animationName = 'reversechangesize';
-    box.style.width='300px';
-    box.style.height='400px';
-    title.style.display='none';
-    buttons.style.display = 'none';
-    title.className="";
-}
 function dropdownmenu()
 {
     if(nav.style.top=="-44vh")

26af98e5ef76274bb78f80062a65e13994bc236a
farrokh03 <140150581+farrokh03@users.noreply.github.com>
2023-08-06 19:31:49 +0330

    1
    رفع مشکل رسپانسیو

--- moreblog/blog.js
+++ moreblog/blog.js
@@ -4,6 +4,7 @@ let buttons = document.getElementById('buttons');
 let menu = document.getElementById('menu');
 let nav = document.getElementById('navbar');
 
+
 menu.onclick = dropdownmenu;
 
 function dropdownmenu()
@@ -11,11 +12,16 @@ function dropdownmenu()
     if(nav.style.top=="-44vh")
     {
         
-        nav.style.top='8vh';
-        nav.style.display='flex';
+        nav.style.top='0';
+        nav.style.zIndex='1';
+        nav.style.position='relative';
+
+
     }
     else{
         nav.style.top='-44vh';
-        nav.style.display='none';
+        nav.style.zIndex='0';
+        nav.style.position='absolute';
+
     }
 }

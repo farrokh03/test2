c460fbc00479d659208b978cac342b5fa4658783
farrokh03 <140150581+farrokh03@users.noreply.github.com>
2023-08-14 11:58:36 +0330

    asd
    sad

--- js/more.js
+++ js/more.js
@@ -4,6 +4,11 @@ let color = document.getElementById('color');
 let navbar = document.getElementById('nav');
 const link = document.getElementById('theme');
 const bgphoto = document.getElementById('background').src;
+let menu = document.getElementById('menu');
+let sidenav = document.getElementById('sidenavbar');
+
+
+menu.onclick = sidemenu;
 
 document.getElementById('photo').style.backgroundImage ='url(' + bgphoto +')';
 
@@ -24,5 +29,16 @@ function switchthem()
         ball.style.animationName = 'reverseball';
         icon.className = "fa-solid fa-sun";
         link.href = "/css/more.css";
+    }
+}
+function sidemenu (){
+    if(sidenav.style.right=="0px")
+    {
+        sidenav.style.right='-350px';
+        sidenav.style.animationName='closesidemenu';
+    }
+    else{
+        sidenav.style.right='0px'
+        sidenav.style.animationName='opensidemenu';
     }
 }

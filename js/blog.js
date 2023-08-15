50ffa627d3660a1c8ea3a488ac3e8b1ef2b23045
farrokh03 <140150581+farrokh03@users.noreply.github.com>
2023-08-15 15:44:38 +0330

    sda
    asdas

--- js/blog.js
+++ js/blog.js
@@ -3,11 +3,17 @@ let ball = document.getElementById('ball');
 let icon = document.getElementById('icon');
 const link = document.getElementById('theme');
 
+
+let click = document.getElementById('show');
+let list =document.getElementById('navbar');
+
+
 let menu = document.getElementById('menu');
 let sidenav = document.getElementById('sidenavbar');
 
 ball.onclick = switchthem;
 menu.onclick = sidemenu;
+click.onclick = show;
 
 function switchthem()
 {
@@ -34,5 +40,17 @@ function sidemenu (){
     else{
         sidenav.style.right='0px'
         sidenav.style.animationName='opensidemenu';
+    }
+}
+function show()
+{
+    if(list.style.top =="7vh")
+    {
+        document.getElementById('showlist').innerHTML='keyboard_arrow_down';
+        list.style.top='-32vh';
+    }
+    else{
+        list.style.top='7vh';
+        document.getElementById('showlist').innerHTML='expand_less';
     }
 }
